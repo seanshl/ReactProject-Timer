9fa1b3abb22f2f358fd9162231c0eb323b3bdb48
Shiyao <liushiyao@Shiyao.local>
2016-05-31 22:15:39 -0500

    Timer completed

---
+++ app/tests/components/Timer.test.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Timer = require('Timer');
+
+describe('Timer', ()=> {
+	it('should exist', () => {
+		expect(Timer).toExist();
+	});
+
+	it('should start timer on started status', () => {
+		var timer = TestUtils.renderIntoDocument(<Timer />);
+
+		timer.handleStatusChange('started');
+		expect(timer.state.count).toBe(0);
+		setTimeout(() => {
+			expect(timer.state.timerStatus).toBe('started');
+			expect(timer.state.count).toBe(1);
+			done();
+		}, 1001);
+	});
+});
